2998f55c49a7048f3b29174dcd49cde5ed6bb35f
Krishna Nayak <nayak100krishna@gmail.com>
2021-07-09 21:37:46 +0530

    email-validatioin-using Regex

--- emailValidation.js
+++ emailValidation.js
@@ -1,26 +1,35 @@
-let email = document.querySelector(".input-box");
-let btn = document.querySelector(".send-btn");
-let error = document.querySelector(".error-text");
-let errorIcon = document.querySelector(".icon-error");
+let email = document.querySelector("input");
+let btn = document.querySelector("button");
+let error = document.querySelector("#error-text");
+let errorIcon = document.querySelector("#error-icon");
+// console.log(email);
 
-btn.addEventListener("click", function (e) {
-  //   console.log(email.value);
+function validCheck() {
   let mailAddress = email.value;
-  let emailRegex = /[A-za-z0-9_.]+@[A-Za-z]+.[a-z]{2,3}$/;
+  let emailRegex = /[A-za-z0-9_.]{3,}@[A-Za-z]+.[a-z]{2,3}$/;
   let condition = emailRegex.test(mailAddress);
   console.log(condition);
   if (!condition) {
-    error.style.visibility = "visible";
-    errorIcon.style.visibility = "visible";
+    error.classList.remove("hidden");
+    errorIcon.classList.remove("hidden");
+    email.classList.add("border-red-400");
     errorIcon.classList.add("animate__animated", "animate__shakeX");
-    error.classList.add("animate__animated", "animate__shakeX");
+    // error.classList.add("animate__animated", "animate__shakeX");
+    console.log("error ❌");
   } else {
-    error.style.visibility = "hidden";
-    errorIcon.style.visibility = "hidden";
+    error.classList.add("hidden");
+    errorIcon.classList.add("hidden");
+    email.classList.remove("border-red-400");
+    console.log("work ✅");
   }
-  setTimeout(function (e) {
+  setTimeout(function () {
     errorIcon.classList.remove("animate__animated", "animate__shakeX");
-    error.classList.remove("animate__animated", "animate__shakeX");
     console.log("animation-reset ✅");
   }, 1000);
+}
+
+btn.addEventListener("click", validCheck);
+
+email.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") validCheck();
 });
